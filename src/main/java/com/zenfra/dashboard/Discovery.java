bddcf8134d44a7f202ef8e06dc67e3afcc6a2c9c
AjithKumar <ajitheee22@gmail.com>
2022-05-28 14:38:21 +0530

    Discovery cases scripts

--- src/main/java/com/zenfra/dashboard/Discovery.java
+++ src/main/java/com/zenfra/dashboard/Discovery.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.FindBys;
 import org.openqa.selenium.support.PageFactory;
@@ -24,8 +26,12 @@ public class Discovery extends TestBase {
 	@FindBy(xpath = "//a[text()='Discovery']")
 	WebElement discocvery;
 
-	@FindBy(xpath = "//a[text()='VMAX Server']")
-	WebElement vMaxSever;
+	// @FindBy(xpath = "//a[text()='VMAX Server']")
+	@FindBy(xpath = "(//a[@class='un-selected-favview'])[1]")
+	WebElement catergortyReport;
+
+	@FindBy(xpath = "//a[@class='selected-favview']")
+	WebElement selectedReport;
 
 	@FindBy(xpath = "//div[@class='report_grid_data ']")
 	WebElement discoveryReport;
@@ -33,11 +39,38 @@ public class Discovery extends TestBase {
 	@FindBys(@FindBy(xpath = "//div[@aria-colindex='1' and contains(@class, 'ag-cell-last-left-pinned')]"))
 	List<WebElement> reportPinnedElements;
 
-	@FindBys(@FindBy(xpath = "//div[@role='row']/div[@col-id='Initiator Name']"))
-	List<WebElement> colIndicator;
+//	@FindBys(@FindBy(xpath = "//div[@role='row']/div[@col-id='Initiator Name']"))
+	@FindBys(@FindBy(xpath = "//div[@role='row']/div[@col-id='Host Group']"))
+	List<WebElement> sortColumn;
 
-	@FindBy(xpath = "//span[@class='ag-header-cell-text' and text()='Initiator Name' and @ref='eText']")
-	WebElement colmunIndicator;
+	// @FindBy(xpath = "//span[@class='ag-header-cell-text' and text()='Initiator
+	// Name' and @ref='eText']")
+	@FindBy(xpath = "(//span[@class='ag-header-cell-text' and @ref='eText'])[3]")
+	WebElement secondColmun;
+
+	@FindBy(xpath = "(//input[@class='ag-input-field-input ag-text-field-input'])[3]")
+	WebElement filerColmun;
+
+	@FindBy(xpath = "//div[@aria-rowindex='3']/div[@aria-colindex='1' and text()='Total']")
+	WebElement totalColmun;
+
+	@FindBy(xpath = "//span[@class='ag-side-button-label' and text()='Columns']")
+	WebElement hideColumn;
+
+	@FindBys(@FindBy(xpath = "//div[@ref='cbSelect']"))
+	List<WebElement> colmunsList;
+
+	@FindBys(@FindBy(xpath = "//div[@class='ag-column-select-column ag-column-select-indent-0']/span[@class='ag-column-select-column-label']"))
+	List<WebElement> colmunsValues;
+
+	@FindBy(id = "excelExport")
+	WebElement excelExport;
+
+	@FindBys(@FindBy(xpath = "//button[@class='btn btn-primary']"))
+	List<WebElement> excelExportOption;
+	
+	@FindBy(xpath = "//div[@class='s-alert-box-inner']")
+	WebElement messageBox;
 
 	public Discovery() {
 		PageFactory.initElements(driver, this);
@@ -48,9 +81,9 @@ public class Discovery extends TestBase {
 		migraton.click();
 		discocvery.click();
 		testUtilObject.waitforLoadingIcon();
-		testUtilObject.waitforElementClickable(vMaxSever);
+		testUtilObject.waitforElementClickable(catergortyReport);
 		Thread.sleep(500);
-		vMaxSever.click();
+		catergortyReport.click();
 		Thread.sleep(500);
 		testUtilObject.waitforLoadingIcon();
 	}
@@ -69,16 +102,24 @@ public class Discovery extends TestBase {
 		}
 	}
 
+	public void verifyTotalColmun() {
+		testUtilObject.waitforElementClickable(totalColmun);
+		if (totalColmun.isDisplayed()) {
+			Assert.assertTrue(true, "Report first first clomun is reserved for total");
+		} else {
+			Assert.assertTrue(false, "Report first first clomun is reserved for total");
+		}
+	}
+
 	public void verifyAscending() throws InterruptedException {
-		colmunIndicator.click();
+		secondColmun.click();
 		Thread.sleep(500);
 		ArrayList<String> obtainedList = new ArrayList<>();
-		System.out.println(colIndicator.size());
-		if (colIndicator.size() > 0) {
+		if (sortColumn.size() > 0) {
 
-			for (WebElement we : colIndicator) {
+			for (WebElement we : sortColumn) {
 				if (!we.getText().trim().equalsIgnoreCase("")) {
-					if (!we.getText().trim().equalsIgnoreCase("Initiator Name")) {
+					if (!we.getText().trim().equalsIgnoreCase("Host Group")) {
 						obtainedList.add(we.getText());
 
 					}
@@ -90,7 +131,6 @@ public class Discovery extends TestBase {
 				sortedList.add(s);
 			}
 			Collections.sort(sortedList);
-		
 
 			Assert.assertTrue(sortedList.equals(obtainedList), "columns sort");
 		} else {
@@ -99,15 +139,16 @@ public class Discovery extends TestBase {
 	}
 
 	public void verifyDescending() throws InterruptedException {
-		colmunIndicator.click();
+		secondColmun.click();
 		Thread.sleep(500);
 		ArrayList<String> obtainedList = new ArrayList<>();
-		if (colIndicator.size() > 0) {
+		if (sortColumn.size() > 0) {
 
-			for (WebElement we : colIndicator) {
-				if (!we.getText().trim().equalsIgnoreCase("Initiator Name")) {
-					obtainedList.add(we.getText());
-
+			for (WebElement we : sortColumn) {
+				if (!we.getText().trim().equalsIgnoreCase("")) {
+					if (!we.getText().trim().equalsIgnoreCase("Host Group")) {
+						obtainedList.add(we.getText());
+					}
 				}
 			}
 			ArrayList<String> sortedList = new ArrayList<>();
@@ -120,6 +161,95 @@ public class Discovery extends TestBase {
 		} else {
 			Assert.assertTrue(false, "columns sort");
 		}
+	}
+
+	public void filerAndVerifyRecord() throws InterruptedException {
+		filerColmun.sendKeys("rip00");
+		Thread.sleep(500);
+		List<WebElement> filteredColumn = driver.findElements(By.xpath("//div[@role='row']/div[@col-id='Host Group']"));
+		ArrayList<String> filteredList = new ArrayList<>();
+		if (filteredColumn.size() > 0) {
+
+			for (int i = 1; i <= filteredColumn.size(); i++) {
+				WebElement we = driver
+						.findElement(By.xpath("(//div[@role='row']/div[@col-id='Host Group'])[" + i + "]"));
+				if (!we.getText().trim().equalsIgnoreCase("")) {
+					if (!we.getText().trim().equalsIgnoreCase("Host Group")) {
+						filteredList.add(we.getText());
+					}
+				}
+			}
+		}
+		boolean matchfalg = false;
+		for (String s : filteredList) {
+			if (s.contains("rip00")) {
+				matchfalg = true;
+			} else {
+				matchfalg = false;
+			}
+
+		}
+		Assert.assertTrue(matchfalg, "verify filter records");
+	}
+
+	public void hideComuns() throws InterruptedException {
+		String hiddenColmun = "";
+		hideColumn.click();
+		hiddenColmun = colmunsValues.get(0).getText().trim();
+		colmunsList.get(0).click();
+		if (driver.findElements(
+				By.xpath("//div[@class='ag-header-cell-label']/span[@class='ag-header-cell-text' and text()='"
+						+ hiddenColmun + "']"))
+				.size() < 1) {
+
+			Assert.assertTrue(true, "Selected Colmun " + hiddenColmun + " hidden");
+			Thread.sleep(500);
+			Actions act = new Actions(driver);
+			act.moveToElement(selectedReport).click().build().perform();
+			selectedReport.click();
+			Thread.sleep(500);
+			testUtilObject.waitforLoadingIcon();
+			if (driver.findElements(
+					By.xpath("//div[@class='ag-header-cell-label']/span[@class='ag-header-cell-text' and text()='"
+							+ hiddenColmun + "']"))
+					.size() < 1) {
+
+				Assert.assertTrue(true, "Selected Colmun " + hiddenColmun + " hidden on relaod");
+				colmunsList.get(0).click();
+			} else
+
+			{
+				Assert.assertTrue(false, "Selected Colmun " + hiddenColmun + " hidden on relaod");
+
+			}
+
+		} else
+
+		{
+			Assert.assertTrue(false, "Selected Colmun " + hiddenColmun + " hidden");
+
+		}
+		hideColumn.click();
+
+	}
+
+	public void verifyExcelExportOptions() {
+		excelExport.click();
+		ArrayList<String> excelOptions = new ArrayList<>();
+		for (WebElement we : excelExportOption) {
+			excelOptions.add(we.getText().trim());
+		}
+		Assert.assertTrue(
+				excelOptions.get(0).equalsIgnoreCase("All Rows All Columns")
+						&& excelOptions.get(1).equalsIgnoreCase("All Rows Visible Columns")
+						&& excelOptions.get(2).equalsIgnoreCase("Visible Rows All Columns")
+						&& excelOptions.get(3).equalsIgnoreCase("Visible Rows Visible Columns"),
+				"Excel Export Options");
+	}
+	
+	public void downloadAsExcel() {
+		excelExportOption.get(0).click();
+		Assert.assertEquals("All Rows All Columns exported successfully", messageBox.getText());
 	}
 
 }
--- src/test/java/com/zenfradashboard/testcases/DiscoveryTest.java
+++ src/test/java/com/zenfradashboard/testcases/DiscoveryTest.java
@@ -14,7 +14,7 @@ public class DiscoveryTest extends TestBase {
 	AzenfraHome homePage;
 	Discovery discovery;
 
-	public DiscoveryTest(){
+	public DiscoveryTest() {
 		super();
 	}
 
@@ -25,24 +25,52 @@ public class DiscoveryTest extends TestBase {
 		discovery = new Discovery();
 		homePage = loginPage.Login(prop.getProperty("username"), prop.getProperty("password"));
 	}
-	@Test (priority = 1)
+
+	@Test(priority = 1)
 	public void verifyReportLoading() throws InterruptedException {
 		discovery.navigateToAnalyticsReport();
 		discovery.verifyReport();
 	}
-	@Test (priority = 2)
+
+	@Test(priority = 2)
+	public void verifyToatalColmun() throws InterruptedException {
+		discovery.verifyTotalColmun();
+	}
+	@Test(priority = 3)
 	public void verifyPinnedState() throws InterruptedException {
 		discovery.verifyPinnedState();
 	}
-	@Test (priority = 3)
+
+	@Test(priority = 4)
 	public void verifyColSortAscending() throws InterruptedException {
 		discovery.verifyAscending();
 	}
-	
-	@Test (priority = 4)
+
+	@Test(priority = 5)
 	public void verifyColSortDecending() throws InterruptedException {
 		discovery.verifyDescending();
 	}
+
+	@Test(priority = 6)
+	public void verifyFilterRecords() throws InterruptedException {
+		discovery.filerAndVerifyRecord();
+	}
+	
+	@Test(priority = 7)
+	public void hideColmunsAndVerify() throws InterruptedException {
+		discovery.hideComuns();
+	}
+	
+
+	@Test(priority = 8)
+	public void verifyExcelExportOptions() throws InterruptedException {
+		discovery.verifyExcelExportOptions();
+	}
+	@Test(priority = 9)
+	public void exportDataAsExccel() throws InterruptedException {
+		discovery.downloadAsExcel();
+	}
+
 	@AfterTest
 	public void tearDown() {
 		driver.close();
